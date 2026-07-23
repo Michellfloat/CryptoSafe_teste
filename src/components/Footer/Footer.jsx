7f05cf1228c96b2ff0465f118c8a8f9eaf4c16d9
michellfloat <michell.ssfloat@gmail.com>
2026-07-23 13:52:24 -0300

    feat:Adding Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer(){
+    return(
+        <footer>
+            <p> &copy {new Date().getFullYear()} CryptoSafe - Todos os Direitos reservados</p>
+
+            <p>Segurança de Ponta a Ponta(Padrão End-to-End Encryption). Seus segredos nunca serão passados 🤫</p>
+        </footer>
+    );
+}
