dae086d81658f1150f1c2e068f03a0261e4a0ce2
michellfloat <michell.ssfloat@gmail.com>
2026-07-27 11:50:42 -0300

    chore: remover node_modules do repositório

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,18 @@
 
+import "./style.css"
 export default function Footer(){
     return(
-        <footer>
-            <p> &copy {new Date().getFullYear()} CryptoSafe - Todos os Direitos reservados</p>
+        <footer className="vault-footer">
+            <div className="footer-container">
+                <div className="security-badge">
+                    <span className="badge-pulse"></span>
+                    
+                    <span>Criptografia Ativa: AES-256 (Zero-Knowledge)</span>
+                </div>
+                    <p className="footer-copyright"> &copy {new Date().getFullYear()} <strong>CryptoSafe</strong> - Todos os Direitos reservados</p>
 
-            <p>Segurança de Ponta a Ponta(Padrão End-to-End Encryption). Seus segredos nunca serão passados 🤫</p>
+                    <p className="footer-subtext">Segurança de Ponta a Ponta. Seus segredos são cifrados no navegador e nunca nunca saem desprotegidos. 🤫</p>
+            </div>
         </footer>
     );
 }
